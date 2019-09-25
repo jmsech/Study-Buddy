43af09dfdde10a8ffaa8216c5cafa0b44f3e5245
bfremin <brandon.fremin@grantsolutions.gov>
2019-09-24 22:43:11 -0400

    Added basic infrastructure to code placing getters and setters

--- src/main/java/com/studybuddy/models/ProjectEvent.java
+++ src/main/java/com/studybuddy/models/ProjectEvent.java
@@ -1,5 +1,6 @@
 package com.studybuddy.models;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ProjectEvent extends AssessmentEvent{
@@ -7,4 +8,26 @@ public class ProjectEvent extends AssessmentEvent{
     private List<User> group;
     private List<Event> meetings;
 
+    public ProjectEvent(LocalDateTime time, String description, int id, double weight, ParticularCourse course,
+                        List<User> group, List<Event> meetings) {
+        super(time, description, id, weight, course);
+        this.group = group;
+        this.meetings = meetings;
+    }
+
+    public List<User> getGroup() {
+        return group;
+    }
+
+    public void setGroup(List<User> group) {
+        this.group = group;
+    }
+
+    public List<Event> getMeetings() {
+        return meetings;
+    }
+
+    public void setMeetings(List<Event> meetings) {
+        this.meetings = meetings;
+    }
 }
--- src/main/java/com/studybuddy/models/StudyEvent.java
+++ src/main/java/com/studybuddy/models/StudyEvent.java
@@ -1,6 +1,7 @@
 package com.studybuddy.models;
 
 import javax.xml.stream.Location;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class StudyEvent extends Event {
@@ -11,6 +12,54 @@ public class StudyEvent extends Event {
     private User host;
     private double importance;
 
+    public StudyEvent(LocalDateTime time, String description, int id, Location place,
+                      List<User> peopleAttending, List<User> pendingInvitations,
+                      User host, double importance) {
+        super(time, description, id);
+        this.place = place;
+        this.peopleAttending = peopleAttending;
+        this.pendingInvitations = pendingInvitations;
+        this.host = host;
+        this.importance = importance;
+    }
 
+    public Location getPlace() {
+        return place;
+    }
 
+    public void setPlace(Location place) {
+        this.place = place;
+    }
+
+    public List<User> getPeopleAttending() {
+        return peopleAttending;
+    }
+
+    public void setPeopleAttending(List<User> peopleAttending) {
+        this.peopleAttending = peopleAttending;
+    }
+
+    public List<User> getPendingInvitations() {
+        return pendingInvitations;
+    }
+
+    public void setPendingInvitations(List<User> pendingInvitations) {
+        this.pendingInvitations = pendingInvitations;
+    }
+
+    public User getHost() {
+        return host;
+    }
+
+    public void setHost(User host) {
+        this.host = host;
+    }
+
+    public double getImportance() {
+        return importance;
+    }
+
+    public void setImportance(double importance) {
+        this.importance = importance;
+    }
 }
