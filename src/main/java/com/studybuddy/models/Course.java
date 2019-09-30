7af6a649bb68128a802e2102e1b29182a1c14b78
Vinicius Lepca <vinicius.lepca@gmail.com>
2019-09-30 14:52:09 -0400

    Updated Course.java - added potentialTutors field

--- src/main/java/com/studybuddy/models/Course.java
+++ src/main/java/com/studybuddy/models/Course.java
@@ -9,21 +9,33 @@ public class Course implements Cloneable{
     private String id;
     private String courseNumber;
     private List<User> alumni;
+    private List<User> potentialTutors;
     private List<ParticularCourse> semesters;
     private List<ParticularCourse> activeClasses;
     private Map<User, String> grades;
     private LocalDateTime time;
 
-    public Course(String id, String courseNumber, List<User> alumni, List<ParticularCourse> semesters,
-                  List<ParticularCourse> activeClasses, Map<User, String> grades, LocalDateTime time) {
+    public Course(String id, String courseNumber, List<User> alumni, List<User> potentialTutors,
+                  List<ParticularCourse> semesters, List<ParticularCourse> activeClasses,
+                  Map<User, String> grades, LocalDateTime time) {
         this.id = id;
         this.courseNumber = courseNumber;
         this.alumni = alumni;
+        this.potentialTutors = potentialTutors;
         this.semesters = semesters;
         this.activeClasses = activeClasses;
         this.grades = grades;
-        this.time =  time;
+        this.time = time;
     }
+
+    public List<User> getPotentialTutors() {
+        return potentialTutors;
+    }
+
+    public void setPotentialTutors(List<User> potentialTutors) {
+        this.potentialTutors = potentialTutors;
+    }
+
     public String getId() {
         return id;
     }
