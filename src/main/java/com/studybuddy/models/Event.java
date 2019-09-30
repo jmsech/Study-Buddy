9a48eef4e06ff0f4327695a70b13630067f1795a
Justin Sech <justinsech@gmail.com>
2019-09-30 14:49:23 -0400

    started implementation

--- src/main/java/com/studybuddy/models/Event.java
+++ src/main/java/com/studybuddy/models/Event.java
@@ -1,45 +1,73 @@
 package com.studybuddy.models;
 
 import java.time.LocalDateTime;
-
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Set;
 
 public class Event {
 
-    private LocalDateTime time; // TODO - Fix This
+    private Date date; // TODO - Fix This
     private String description;
     private int id;
+    private Set<Integer> hosts;
 
-    public Event(LocalDateTime time, String description, int id) {
-        this.time = time;
+    public Event(Date date, String description, int id, Set<Integer> hosts) {
+        this.date = date;
         this.description = description;
         this.id = id;
+        this.hosts = hosts;
     }
 
     public void addToCalendar(String calendarID) {
-        // TODO
+        // TODO ?
     }
 
-//    public LocalDateTime getTime() {
-//        return time;
-//    }
-//
-//    public void setTime(LocalDateTime time) {
-//        this.time = time;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
+    public int compareTo(Event e) {
+        return this.date.compareTo(e.getDate());
+    }
+
+    public boolean equals(Event e) {
+        return this.id == e.getId();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<Integer> getHosts() {
+        return this.hosts;
+    }
+
+    public void removeHost(int hostid) {
+        this.hosts.remove(hostid);
+    }
+
+    public void addHost(int hostid) {
+        this.hosts.add(hostid);
+    }
+
+    public boolean isHost(int id) {
+        return hosts.contains(id);
+    }
 }
--- src/main/java/com/studybuddy/models/StudyEvent.java
+++ src/main/java/com/studybuddy/models/StudyEvent.java
@@ -1,67 +1,86 @@
 package com.studybuddy.models;
 
 import javax.xml.stream.Location;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+
+import static java.lang.Double.compare;
 
 public class StudyEvent extends Event{
 
     private Location place; // TODO - Fix This
     private List<User> peopleAttending;
     private List<User> pendingInvitations;
-    private User host;
     private double importance;
 
-    public StudyEvent(LocalDateTime time, String description, int id, Location place,
+    public StudyEvent(Date date, String description, int id, Location place,
                       List<User> peopleAttending, List<User> pendingInvitations,
-                      User host, double importance) {
-        super(time, description, id);
+                      Set<Integer> hosts, double importance) {
+        super(date, description, id, hosts);
         this.place = place;
         this.peopleAttending = peopleAttending;
         this.pendingInvitations = pendingInvitations;
-        this.host = host;
         this.importance = importance;
     }
 
-    public void addUser(User user) { /*TODO*/}
+    public void inviteUser(User user) {
+        this.pendingInvitations.add(user);
+        //TODO send invite
+    }
 
-//    public Location getPlace() {
-//        return place;
-//    }
-//
-//    public void setPlace(Location place) {
-//        this.place = place;
-//    }
-//
-//    public List<User> getPeopleAttending() {
-//        return peopleAttending;
-//    }
-//
-//    public void setPeopleAttending(List<User> peopleAttending) {
-//        this.peopleAttending = peopleAttending;
-//    }
+    public void markAttending(User user) {
+        this.peopleAttending.add(user);
+        this.pendingInvitations.remove(user);
+    }
 
-//    public List<User> getPendingInvitations() {
-//        return pendingInvitations;
-//    }
-//
-//    public void setPendingInvitations(List<User> pendingInvitations) {
-//        this.pendingInvitations = pendingInvitations;
-//    }
-//
-//    public User getHost() {
-//        return host;
-//    }
-//
-//    public void setHost(User host) {
-//        this.host = host;
-//    }
-//
-//    public double getImportance() {
-//        return importance;
-//    }
-//
-//    public void setImportance(double importance) {
-//        this.importance = importance;
-//    }
+    public void removeAttendee(User user) {
+        this.peopleAttending.remove(user);
+    }
+
+    public int compareImportance(StudyEvent e) {
+        return compare(this.importance, e.getImportance());
+    }
+
+    public boolean deleteStudyEvent(int id) {
+        if (this.isHost(id)) {
+            this.peopleAttending.clear();
+            this.pendingInvitations.clear();
+            // TODO notify users?
+            return true;
+        }
+        return false;
+    }
+
+    public Location getPlace() {
+        return place;
+    }
+
+    public void setPlace(Location place) {
+        this.place = place;
+    }
+
+    public List<User> getPeopleAttending() {
+        return peopleAttending;
+    }
+
+    public void setPeopleAttending(List<User> peopleAttending) {
+        this.peopleAttending = peopleAttending;
+    }
+
+    public List<User> getPendingInvitations() {
+        return pendingInvitations;
+    }
+
+    public void setPendingInvitations(List<User> pendingInvitations) {
+        this.pendingInvitations = pendingInvitations;
+    }
+
+    public double getImportance() {
+        return importance;
+    }
+
+    public void setImportance(double importance) {
+        this.importance = importance;
+    }
 }
