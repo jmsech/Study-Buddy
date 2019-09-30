052a2e97622e8a875c6bb531a5cf3bd9e383adfc
moore2118 <mmoore2118@gmail.com>
2019-09-30 15:48:48 -0400

    defined functions in AssessmentEvent

--- src/main/java/com/studybuddy/models/AssessmentEvent.java
+++ src/main/java/com/studybuddy/models/AssessmentEvent.java
@@ -1,7 +1,10 @@
 package com.studybuddy.models;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+
+import static java.lang.Double.compare;
 
 public class AssessmentEvent extends Event{
 
@@ -9,35 +12,47 @@ public class AssessmentEvent extends Event{
     private ParticularCourse course;
     private List<Event> studyEvents;
 
-    public AssessmentEvent(LocalDateTime time, String description, int id, double weight, ParticularCourse course) {
-        super(time, description, id);
+    public AssessmentEvent(Date date, String description, int id, Set<Integer> hosts, double weight, ParticularCourse course) {
+        super(date, description, id, hosts);
         this.weight = weight;
         this.course = course;
     }
 
     public void remind() { /*TODO*/ }
 
-//    public double getWeight() {
-//        return weight;
-//    }
-//
-//    public void setWeight(double weight) {
-//        this.weight = weight;
-//    }
-//
-//    public ParticularCourse getCourse() {
-//        return course;
-//    }
-//
-//    public void setCourse(ParticularCourse course) {
-//        this.course = course;
-//    }
-//
-//    public List<Event> getStudyEvents() {
-//        return studyEvents;
-//    }
-//
-//    public void setStudyEvents(List<Event> studyEvents) {
-//        this.studyEvents = studyEvents;
-//    }
+    public int compareWeight(AssessmentEvent e1) {
+        return compare(this.weight, e1.getWeight());
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public ParticularCourse getCourse() {
+        return course;
+    }
+
+    public void setCourse(ParticularCourse course) {
+        this.course = course;
+    }
+
+    public List<Event> getStudyEvents() {
+        return studyEvents;
+    }
+
+    public void setStudyEvents(List<Event> studyEvents) {
+        this.studyEvents = studyEvents;
+    }
+
+    public void deleteStudyEvents(int id, Event e1) {
+        if (this.isHost(id)) {
+            studyEvents.remove(e1);
+        }
+        return;
+    }
+
 }
