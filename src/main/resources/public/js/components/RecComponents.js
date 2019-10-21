35e26e1e813e705c2ffcaf938377813393697547
Justin Sech <justinsech@gmail.com>
2019-10-21 01:02:05 -0400

    suggest event button working!!!! and new colorful

--- src/main/resources/public/js/components/RecComponents.js
+++ src/main/resources/public/js/components/RecComponents.js
@@ -5,14 +5,18 @@ class Recommendation extends React.Component {
     }
 
     async getRec() {
-        this.setState({recs: await (await fetch(`/${this.props.userID}/recs`)).json()});
+        this.setState({recs: [await (await fetch(`/${this.props.userID}/recs`)).json()]});
+    }
+
+    clearRecs() {
+        this.setState({recs: []});
     }
 
     render() {
         return (
             <div>
                 <NewRecButton getRec = {this.getRec.bind(this)}/>
-                <RecList userID={this.props.userID} recs = {this.state.recs}/>
+                <RecList userID={this.props.userID} recs={this.state.recs} clearRecs={this.clearRecs.bind(this)}/>
             </div>
         )
     }
@@ -21,21 +25,21 @@ class Recommendation extends React.Component {
 class NewRecButton extends React.Component {
     render() {
         let title = "Generate a Recommendation";
-        return <button className={this.props.className} onClick={() => { this.props.getRec() }}>{title}</button>;
+        return <button className="btn" onClick={() => { this.props.getRec() }}>{title}</button>;
     }
 }
 
 class Rec extends React.Component {
     render() {
         return (
-            <li className="card hoverable teal lighten-2"> /*  */
+            <li className="card hoverable cyan darken-2">
                 <div className="card-content black-text">
                     <span className="card-title"> <EventTitle event={this.props.event}/></span>
                     <EventDescription event={this.props.event}/>
                     <EventDateTime event={this.props.event}/>
                 </div>
                 <div className="card-action right-align">
-                    <DeleteButton event={this.props.event} userID = {this.props.userID}/>
+                    <RecDeleteButton event={this.props.event} userID = {this.props.userID} clearRecs={this.props.clearRecs}/>
                 </div>
             </li>
         );
@@ -46,7 +50,20 @@ class RecList extends React.Component {
     render() {
         return <div>
             <h3>Here are your recommendations:</h3>
-            <ul>{this.props.recs.map(rec => <Event key={rec.id} event={rec} userID = {this.props.userID}/>)}</ul>
+            <ul>{this.props.recs.map(rec => <Rec key={rec.id} event={rec} userID={this.props.userID} clearRecs={this.props.clearRecs}/>)}</ul>
         </div>;
+    }
+}
+
+class RecDeleteButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = null;
+    }
+
+    render() {
+        return (
+            <button className="btn cyan darken-3" onClick={() => {this.props.clearRecs()}}><i className="material-icons">delete</i></button>
+        )
     }
 }
