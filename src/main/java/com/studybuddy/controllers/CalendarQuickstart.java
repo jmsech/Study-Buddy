60c878884da9580fcccefe94dabbe04c2dbf1146
Justin Sech <justinsech@gmail.com>
2019-11-04 16:10:05 -0500

    google calender prompts user to login each time

--- src/main/java/com/studybuddy/controllers/CalendarQuickstart.java
+++ src/main/java/com/studybuddy/controllers/CalendarQuickstart.java
@@ -61,7 +61,6 @@ public class CalendarQuickstart {
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
