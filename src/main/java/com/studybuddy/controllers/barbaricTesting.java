419575705dfd90c22fbbdc21c599cc3919bbd51a
bfremin <brandon.fremin@grantsolutions.gov>
2019-11-05 21:50:19 -0500

    Made code cleaner and broke code up into main function, helper functions, and constants.

--- src/main/java/com/studybuddy/controllers/barbaricTesting.java
+++ src/main/java/com/studybuddy/controllers/barbaricTesting.java
@@ -13,41 +13,35 @@ import java.util.Comparator;
 
 public class barbaricTesting {
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // CONSTANTS ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     private static final int COMPRESSION_FACTOR = 5;
     private static final int SECONDS_PER_MINUTE = 60 * COMPRESSION_FACTOR; //seconds/60 = minutes
     private static final long SECONDS_PER_DAY = 86400;
     private static final long SECONDS_OF_SLEEP = 28800;
     private static final int MINUTES_PER_HOUR = (int) (SECONDS_PER_DAY/SECONDS_PER_MINUTE/24);
 
-    public static void main(String[] args) {
-        ArrayList<TimeChunk> list = new ArrayList<>();
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // MAIN FUNCTION ///////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        list.add(new TimeChunk(
-                LocalDateTime.of(2020,1,1,0,0),
-                LocalDateTime.of(2020,1,1,12,0))); // 01/01/2020 12am-12pm
-        list.add(new TimeChunk(
-                LocalDateTime.of(2020,1,1,17,0),
-                LocalDateTime.of(2020,1,1,20,0))); // 01/01/2020 5pm-8pm
-        list.add(new TimeChunk(
-                LocalDateTime.of(2020,1,2,14,0),
-                LocalDateTime.of(2020,1,2,20,0))); // 01/02/2020 2pm-8pm
-        list.add(new TimeChunk(
-                LocalDateTime.of(2020,1,2,18,0),
-                LocalDateTime.of(2020,1,2,22,0))); // 01/02/2020 6pm-10pm
-        list.add(new TimeChunk(
-                LocalDateTime.of(2020,1,7,18,0),
-                LocalDateTime.of(2020,1,8,2,0))); // 01/07/2020 6pm - 01/08/2020 2am
+    public static void main(String[] args) {
+        ArrayList<TimeChunk> list = getTestingList();
+
         LocalDateTime start = LocalDateTime.of(2020,1,1,0,0); // 01/01/2020 12am
         LocalDateTime end = LocalDateTime.of(2020,1,7,23,59); // 01/07/2020 11:59pm
+
         ArrayList<TimeChunk> chunks = makeRecommendation(start, end, list, 3);
         for (var chunk : chunks) {
             System.out.println(chunk.getStartTime() + " " + chunk.getEndTime());
         }
     }
 
-    private static LocalDateTime makeTime(long t) {
-        return LocalDateTime.ofEpochSecond(t,0,ZoneOffset.ofHours(0));
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // CRITICAL FUNCTIONS //////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private static ArrayList<TimeChunk> makeRecommendation(LocalDateTime start, LocalDateTime end, ArrayList<TimeChunk> unavailable, double fraction) {
 
@@ -104,13 +98,17 @@ public class barbaricTesting {
             } else if (freeTime[i] == 0 && state != -1)  {
                 chunks.add(new TimeChunk(
                         makeTime(startSec + (state)*SECONDS_PER_MINUTE),
-                        makeTime(startSec + (i)*SECONDS_PER_MINUTE)));
+                        makeTime(startSec + (i)*SECONDS_PER_MINUTE)
+                ));
                 state = -1;
             }
         }
-        if (state != -1) { chunks.add(new TimeChunk(
+        if (state != -1) {
+            chunks.add(new TimeChunk(
                 makeTime(startSec + state*SECONDS_PER_MINUTE),
-                makeTime(startSec + (length - 1)*SECONDS_PER_MINUTE))); }
+                makeTime(startSec + (length - 1)*SECONDS_PER_MINUTE)
+            ));
+        }
 
         return createStudyChunks(chunks, fraction);
     }
@@ -133,14 +131,16 @@ public class barbaricTesting {
                     long forwardEnd = start + (i+1)*studyLength;
                     TimeChunk c = new TimeChunk(
                             makeTime((forwardBegin-1)*SECONDS_PER_MINUTE),
-                            makeTime((forwardEnd-1)*SECONDS_PER_MINUTE));
+                            makeTime((forwardEnd-1)*SECONDS_PER_MINUTE)
+                    );
                     studyChunks.add(c);
 
                     long reverseEnd = end - i*studyLength;
                     long reverseBegin = end - (i+1)*studyLength;
                     c = new TimeChunk(
                             makeTime(reverseBegin*SECONDS_PER_MINUTE),
-                            makeTime(reverseEnd*SECONDS_PER_MINUTE));
+                            makeTime(reverseEnd*SECONDS_PER_MINUTE)
+                    );
                     studyChunks.add(c);
                 }
                 if (numSlots % 2 == 1) {
@@ -149,7 +149,8 @@ public class barbaricTesting {
                     long forwardEnd = start + (i+1)*studyLength;
                     TimeChunk c = new TimeChunk(
                             makeTime((forwardBegin-1)*SECONDS_PER_MINUTE),
-                            makeTime((forwardEnd-1)*SECONDS_PER_MINUTE));
+                            makeTime((forwardEnd-1)*SECONDS_PER_MINUTE)
+                    );
                     studyChunks.add(c);
                 }
             }
@@ -169,5 +170,42 @@ public class barbaricTesting {
         studyChunks.sort(new TimeChunkComparator());
 
         return studyChunks;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // HELPER FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static LocalDateTime makeTime(long t) {
+        return LocalDateTime.ofEpochSecond(t,0,ZoneOffset.ofHours(0));
+    }
+
+    private static long getStartSec(TimeChunk t) {
+        return t.getStartTime().toEpochSecond(ZoneOffset.UTC);
+    }
+
+    private static long getEndSec(TimeChunk t) {
+        return t.getEndTime().toEpochSecond(ZoneOffset.UTC);
+    }
+
+    private static ArrayList<TimeChunk> getTestingList() {
+        ArrayList<TimeChunk> list = new ArrayList<>();
+        list.add(new TimeChunk(
+                LocalDateTime.of(2020,1,1,0,0),
+                LocalDateTime.of(2020,1,1,12,0))); // 01/01/2020 12am-12pm
+        list.add(new TimeChunk(
+                LocalDateTime.of(2020,1,1,17,0),
+                LocalDateTime.of(2020,1,1,20,0))); // 01/01/2020 5pm-8pm
+        list.add(new TimeChunk(
+                LocalDateTime.of(2020,1,2,14,0),
+                LocalDateTime.of(2020,1,2,20,0))); // 01/02/2020 2pm-8pm
+        list.add(new TimeChunk(
+                LocalDateTime.of(2020,1,2,18,0),
+                LocalDateTime.of(2020,1,2,22,0))); // 01/02/2020 6pm-10pm
+        list.add(new TimeChunk(
+                LocalDateTime.of(2020,1,7,18,0),
+                LocalDateTime.of(2020,1,8,2,0))); // 01/07/2020 6pm - 01/08/2020 2am
+        return list;
     }
 }
