b816ad216645182bc860fac23719b252c3ea8ba6
Sara Weill <saracweill@gmail.com>
2019-11-26 17:36:25 -0500

    backend for getting a recommendation to study with new buddies in your class added two new databases for tracking courses

--- src/main/java/com/studybuddy/controllers/ApplicationController.java
+++ src/main/java/com/studybuddy/controllers/ApplicationController.java
@@ -26,6 +26,8 @@ public class ApplicationController {
         statement.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, hashedPassword TEXT, hashSalt TEXT, firstName TEXT, lastName TEXT)");
         statement.execute("CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, startTime DATETIME, endTime DATETIME, description TEXT, location TEXT, hostId INTEGER, isGoogleEvent BOOLEAN, expired BOOLEAN)");
         statement.execute("CREATE TABLE IF NOT EXISTS events_to_users_mapping (id INTEGER PRIMARY KEY AUTOINCREMENT, eventId INTEGER, userId INTEGER, FOREIGN KEY (eventId) REFERENCES events (id), FOREIGN KEY (userId) REFERENCES users (id))");
+        statement.execute("CREATE TABLE IF NOT EXISTS courses (id INTEGER PRIMARY KEY AUTOINCREMENT, courseNum TEXT, courseSectionNum TEXT, courseName TEXT)");
+        statement.execute("CREATE TABLE IF NOT EXISTS courses_to_users_mapping (id INTEGER PRIMARY KEY AUTOINCREMENT, courseId INTEGER, userId INTEGER, FOREIGN KEY (courseId) REFERENCES courses (id), FOREIGN KEY (userId) REFERENCES users (id))");
         statement.close();
     }
 
