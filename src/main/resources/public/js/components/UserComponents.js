f035fb6aa23d18dde7f832fe1fbeee7f4eb7dc52
Justin Sech <justinsech@gmail.com>
2019-12-11 10:40:26 -0500

    added deadline form

--- src/main/resources/public/js/components/UserComponents.js
+++ src/main/resources/public/js/components/UserComponents.js
@@ -1,21 +1,31 @@
 class User extends React.Component {
+    // Constructor called by application.js
+    constructor(props) {
+        super(props);
+
+        // Determine the userId from the website url. (location.search)
+        const parameters = location.search.substring(1).split("&");
+        const temp = parameters[0].split("=");
+        const id = unescape(temp[1]);
+        this.state = {userID: id};
+    }
     render () {
        return (
            <div>
-               <SeeCourses active={this.props.showCourseDisplay} flip={this.props.flipCourseDisplay} userID={this.props.userId}/>
+               <SeeCourses active={this.props.showCourseDisplay} flip={this.props.flipCourseDisplay} userID={this.state.userID}/>
                <AddCourse flipAddCourse={this.props.flipAddCourseFormState} showAddCourseForm={this.props.showAddCourseForm} userID={this.props.userID}/>
                {/* Below is the original display on the webpage */}
                <div className="content-row">
                    {/* "Column" splits the page up into as many columns as necessary (in this case 2) */}
                    <div className="eventsColumn">
                        {/* Header which says Events and Google Calendar button */}
-                       <h3>Events <GetGoogleEvents userID={this.props.userId}/> </h3>
+                       <h3>Events <GetGoogleEvents userID={this.state.userID}/> </h3>
                        {/* "New Event" Button */}
                        <NewEventButton className="new-event-button btn white-text" flip={this.props.flipEventFormState} showEventForm={this.props.showEventForm}/>
                        {/* New event form (which is hidden when "New Event" is unclicked) */}
-                       <NewEventForm userID={this.props.userId} showEventForm={this.props.showEventForm} flip={this.props.flipEventFormState}/>
+                       <NewEventForm userID={this.state.userID} showEventForm={this.props.showEventForm} flip={this.props.flipEventFormState}/>
                        {/* List of events which user is attending */}
-                       <EventList userID={this.props.userId}/>
+                       <EventList userID={this.state.userID}/>
                    </div>
                    <div className="recColumn">
                        {/* Recommendations Header */}
@@ -24,11 +34,11 @@ class User extends React.Component {
                        <div className="column">
                             <h5 className="center"> Find a time to study with a group of your buddies </h5>
                             {/* Display list of recommendations */}
-                            <Recommendations flipRec={this.props.flipRecFormState} showRecForm={this.props.showRecForm} userID={this.props.userId}/>
+                            <Recommendations flipRec={this.props.flipRecFormState} showRecForm={this.props.showRecForm} userID={this.state.userID}/>
                        </div>
                        <div className="column">
                             <h5 className="center"> Find new buddies to study with in one of your classes </h5>
-                            <BuddyRecommendations flipRec={this.props.flipBuddyRecFormState} showBuddyRecForm={this.props.showBuddyRecForm} userID={this.props.userId}/>
+                            <BuddyRecommendations flipRec={this.props.flipBuddyRecFormState} showBuddyRecForm={this.props.showBuddyRecForm} userID={this.state.userID}/>
                        </div>
                        </div>
                    </div>
@@ -74,12 +84,16 @@ class GetGoogleEvents extends React.Component {
 }
 
 class SeeCourses extends React.Component {
+    constructor(props) {
+        super(props);
+        //this.state = {};
+    }
     render() {
         let text = "See Courses";
         return (
             <div>
                 <button data-target="slide-out" className="btn sidenav-trigger"> {text} </button>
-                <CourseList userID = {this.props.userID}/>
+                <CourseList userID = {this.props.userID} active={this.props.active} flip = {this.props.flip}/>
             </div>
         )
     }
@@ -94,6 +108,9 @@ class CourseList extends React.Component {
     }
 
     async getDataFromServer() {
+        await fetch(`/${this.props.userID}/courses`, {method: "PUT"});
+        {/* FIXME does this line do anthing?*/
+        }
         this.setState({courses: await (await fetch(`/${this.props.userID}/courses`)).json()});
         window.setTimeout(() => {
             this.getDataFromServer();
@@ -109,12 +126,21 @@ class CourseList extends React.Component {
     render() {
         return (
             <div>
-                <ul id="slide-out" className="sidenav">
+                <ul id="slide-out" className="sidenav beige">
                     <span className="card-title">
                         <h4>Your Courses</h4>
                     </span>
-                    {this.state.courses.map(course => <Course key={course.id} course={course}/>)}
+                    {this.state.courses.map(course =>
+                        <Course key={course.id}
+                                course={course}
+                                userID={this.props.userID}
+                                active={this.props.active}
+                                flip = {this.props.flip}
+                        />
+                    )}
                 </ul>
+
+
             </div>
         );
     }
@@ -137,9 +163,11 @@ class Course extends React.Component {
                     <p>{this.props.course.instructor}</p>
                     <p>{this.props.course.timeString}</p>
                     <CourseLocation course={this.props.course}/>
-                    <button className="btn">Add Deadline</button>
+                    <DeadlineButton userID={this.props.userID} active={this.props.active} flip = {this.props.flip}/>
+                    <NewDeadlineForm active={this.props.active} flip = {this.props.flip}/>
                 </div>
             </div>
+
         );
     }
 }
@@ -160,8 +188,94 @@ class CourseLocation extends React.Component {
     }
 }
 
+class DeadlineButton extends React.Component {
+    render() {
+        let title = "Add Deadline";
+        if (this.props.active) {
+            title = "Cancel";
+        }
+        return <button className="btn centralized-button" onClick={() => { this.props.flip() }}>{title}</button>;
+    }
+}
+
+class NewDeadlineForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
 
+    handleSubmit(event) {
+        this.props.flip();
+        const formData = new FormData();
+        formData.append("userId", this.props.userID);
+        formData.append("title", event.target.title.value);
+        // combine tim/date into the format yyyy-mm-dd 00:00
+        formData.append("startTime", event.target.startDate.value + " " + event.target.startTime.value);
+        formData.append("description", event.target.description.value);
+        event.preventDefault();
+        // TODO: Call appropriate path on Server
+        // fetch(`../${this.props.userID}/courses`, {method: "POST", body: formData})
+        event.target.reset(); // clear the form entries
+    }
+
+    componentDidMount() {
+        M.Datepicker.init(document.querySelectorAll('.datepicker'), {
+            autoClose: true,
+            showClearBtn: true,
+            format: "yyyy-mm-dd",
+            minDate: (new Date())
+        });
+        M.Timepicker.init(document.querySelectorAll('.timepicker'), {
+            showClearBtn: true
+        });
+    }
+
+    formatAMPM(hours, minutes) {
+        var ampm = (hours >= 12 && hours < 24) ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? '0'+ minutes : minutes;
+        return hours + ':' + minutes + ' ' + ampm;
+    }
+
+    render() {
+        let style = {display: "none"};
+        if (this.props.active) { style = {display: "block"}};
+        let date = new Date();
+        // Define default start date
+        let defaultStartDate = date.getFullYear()+'-'+(date.getMonth()+1)+'-';
+        if (date.getDate() < 10) {
+            defaultStartDate = defaultStartDate + "0" + date.getDate();
+        } else {
+            defaultStartDate = defaultStartDate + date.getDate();
+        }
+        return (
+            <form id="eventform" onSubmit={this.handleSubmit} style={style}>
+                <div className="input-field">
+                    <label htmlFor="title">Title</label>
+                    <input id="title" name="title" type="text" required/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="description">Description</label>
+                    <textarea id="description" name="description" className="materialize-textarea"/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="startDate" className="active">Due Date</label>
+                    <input id="startDate" type="text" className="datepicker" defaultValue={defaultStartDate} required/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="startTime" className="active">Due Time</label>
+                    <input id="startTime" name="startTime" type="text" className="timepicker"/>
+                </div>
+                <button className="btn white-text">Save Deadline</button>
+            </form>
+        );
+    }
+}
 
 
 
